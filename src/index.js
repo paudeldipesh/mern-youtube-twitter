c63718e3728c86d7a1afef87c867fea51adda249
Dipesh Paudel <thedeepyash7@gmail.com>
2024-03-16 19:08:37 +0545

    add utils code

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ dotenv.config({
   path: "./env",
 });
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+const port = process.env.PORT || 8000;
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App is listening at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed!", error);
+  });
