2599cacabd2ba636f63806d965438f26220a940a
Dipesh Paudel <thedeepyash7@gmail.com>
2024-03-21 20:59:16 +0545

    setup router and controller for registration

--- src/app.js
+++ src/app.js
@@ -15,3 +15,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// Routes import
+import userRouter from "./routes/user.routes.js";
+// Routes declaration
+app.use("/api/v1/users", userRouter);
