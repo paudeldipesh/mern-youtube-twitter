535a9bfa1e383315c476e2a213421632af275884
Dipesh Paudel <thedeepyash7@gmail.com>
2024-03-31 09:43:22 +0545

    add different routes

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -1,10 +1,10 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js";
 
-const healthcheck = asyncHandler(async (_, res) => {
+const healthCheck = asyncHandler(async (_, res) => {
   return res
     .status(200)
     .json(new ApiResponse(200, { message: "Everything is O.K" }, "Ok"));
 });
 
-export { healthcheck };
+export { healthCheck };
