a8f672acfd058df66b80c339d589cb59eba74365
Dipesh Paudel <thedeepyash7@gmail.com>
2024-03-31 09:45:42 +0545

    declare all routes

--- src/app.js
+++ src/app.js
@@ -20,5 +20,22 @@ app.use(morgan("dev"));
 
 // Routes import
 import userRouter from "./routes/user.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import healthCheckRouter from "./routes/healthCheck.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+
 // Routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
